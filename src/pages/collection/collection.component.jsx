52ce9859d818c4938dce6ff2141b2ffbe8a5647d
Vimal Raveendran <vimalraveendra@gmail.com>
2021-04-01 17:36:42 +0200

    created collectionContext to provide the collections into collectionsComponent,also created CurrentUserContext dynamically update the currentUser value in the App component using Provider & localState

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React,{useContext} from 'react';
+// import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+// import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collections/collections.context'
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  
+  const collections= useContext(CollectionsContext)
+  const collection = collections[match.params.collectionId]
   const { title, items } = collection;
+
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
@@ -17,12 +22,12 @@ const CollectionPage = ({ collection }) => {
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
-    </div>
+    </div>  
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+// const mapStateToProps = (state, ownProps) => ({
+//   collection: selectCollection(ownProps.match.params.collectionId)(state)
+// });
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
