a5b48860737d84255a36c7306c78842a876b6ceb
Vimal Raveendran <vimalraveendra@gmail.com>
2021-04-02 10:43:54 +0200

    Created CartContext api to dynamically set the values inside the Heade & CartIcon components to avoid the prop drilling problem

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -12,11 +12,15 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CurrentUserContext from '../../contexts/current-user/current-user.context'
+import CartContext from '../../contexts/cart/cart.context'
 
-import './header.styles.scss';
+import './header.styles.scss';  
 
-const Header = ({hidden }) => {
+const Header = () => {
+  const[hidden,setHidden]=useState(true)
   const currentUser= useContext(CurrentUserContext)
+  const toggleHidden=()=>setHidden(!hidden)
+  
   return(
   <div className='header'>
     <Link className='logo-container' to='/'>
@@ -38,7 +42,12 @@ const Header = ({hidden }) => {
           SIGN IN
         </Link>
       )}
+      <CartContext.Provider value={{
+        hidden,
+        toggleHidden
+      }}>
       <CartIcon />
+      </CartContext.Provider>
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
